374718731541aae8d2789bcbd26149b3b19826c4
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-18 15:00:04 +0600

    adjusted few details

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Navigate to="/category/1"></Navigate>
     </div>
   );
 };
--- src/Components/homelayout/FindUs.jsx
+++ src/Components/homelayout/FindUs.jsx
@@ -8,9 +8,9 @@ const FindUs = () => {
       <h2 className='font-bold mb-5'>Find Us on</h2>
       <div className=''>
         <div className='join join-vertical w-full'>
-          <button className='btn bg-base-100 justify-start join-item'><FaSquareFacebook></FaSquareFacebook> Facebook</button>
-          <button className='btn bg-base-100 justify-start join-item'><FaInstagram></FaInstagram> Instagram</button>
-          <button className='btn bg-base-100 justify-start join-item'><FaTwitter></FaTwitter> Twitter</button>
+          <button className='btn bg-base-100 justify-start join-item'><FaSquareFacebook size={20}></FaSquareFacebook> Facebook</button>
+          <button className='btn bg-base-100 justify-start join-item'><FaInstagram size={20}></FaInstagram> Instagram</button>
+          <button className='btn bg-base-100 justify-start join-item'><FaTwitter size={20}></FaTwitter> Twitter</button>
         </div>
       </div>
     </div>
