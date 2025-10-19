94ca89b8731c7c290f9fe572a1c6d1e6b8349692
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-19 19:12:18 +0600

    Register User & OnAuthStateChange

--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const Register = () => {
+  const { createUser, setUser } = use(AuthContext);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    const form = e.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, photo, password, email);
+    createUser(email, password)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorCode, errorMessage);
+      })
+
+  }
   return (
     <div className='flex justify-center mt-2 items-center w-11/12 mx-auto'>
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
         <h2 className='font-semibold text-2xl text-center'>Register your account</h2>
-        <div className="card-body">
+        <form onSubmit={handleRegister} className="card-body">
           <fieldset className="fieldset">
             <label className="label">Name</label>
-            <input type="text" className="input" placeholder="Name" />
+            <input name='name' type="text" className="input" placeholder="Name" required />
 
             <label className="label">Photo URL</label>
-            <input type="text" className="input" placeholder="Photo URL" />
+            <input name='photo' type="text" className="input" placeholder="Photo URL" required />
 
             <label className="label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
+            <input name='email' type="email" className="input" placeholder="Email" required />
 
             <label className="label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
+            <input name='password' type="password" className="input" placeholder="Password" required />
 
-            <button className="btn btn-neutral mt-4">Register</button>
+            <button type='submit' className="btn btn-neutral mt-4">Register</button>
             <p className='text-center pt-3 font-medium'>Already have an account? <Link className='text-secondary' to='/auth/login'> Login</Link> </p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,32 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { app } from '../Firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from "firebase/auth";
 export const AuthContext = createContext();
+
+const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: "Shafin",
-    email: "sed@des.com"
-  });
+  const [user, setUser] = useState(null);
+
+  console.log(user);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    }
+  }, []);
+
+
   const authData = {
     user,
     setUser,
+    createUser
   }
   return (
     <AuthContext value={authData}>
