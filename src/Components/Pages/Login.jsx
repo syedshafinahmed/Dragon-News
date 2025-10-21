4e0c5919848164ac908178a856cd07e40156224e
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-10-21 22:09:27 +0600

    updated

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     signIn(email, password)
       .then(result => {
         const user = result.user;
-        // console.log(user);
+        console.log(user);
         navigate(`${location.state ? location.state : '/'}`)
       })
       .catch(error => {
